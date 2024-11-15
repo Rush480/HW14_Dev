12a329a443ed453aaecc2e13b3aa866557c8ff36
Rush480 <admin.phoneone@gmail.com>
2024-11-15 14:49:21 +0000

    some fixes in NoteService

--- src/main/java/org/app/hw14_dev/service/NoteService.java
+++ src/main/java/org/app/hw14_dev/service/NoteService.java
@@ -2,7 +2,7 @@ package org.app.hw14_dev.service;
 
 
 import org.app.hw14_dev.model.Note;
-import org.app.hw14_dev.repository.InMemoryNoteRepository;
+import org.app.hw14_dev.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +11,10 @@ import java.util.List;
 @Service
 public class NoteService {
 
-    private final InMemoryNoteRepository inMemoryNoteRepository;
+    private final NoteRepository inMemoryNoteRepository;
 
     @Autowired
-    public NoteService(InMemoryNoteRepository inMemorynoteRepository) {
+    public NoteService(NoteRepository inMemorynoteRepository) {
         this.inMemoryNoteRepository = inMemorynoteRepository;
     }
 
